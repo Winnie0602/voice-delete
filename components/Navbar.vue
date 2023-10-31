1aec95e69465ee179d41f3a74b2514aba6bf2eb9
winnie chang <z976200@gmail.com>
2023-10-31 11:34:28 +0800

    新增nuxt 基本配置檔

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul>
+    <li><NuxtLink to="/login">login</NuxtLink></li>
+    <li><NuxtLink to="/">index</NuxtLink></li>
+    <li><NuxtLink to="/private">private</NuxtLink></li>
+  </ul>
+</template>
